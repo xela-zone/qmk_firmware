fb6706004ecb8e1d90c4f77be843b79bb9a67dfb
Xela <git@xela.zone>
2026-08-01 18:34:59 -0800

    chore(devenv): add clang-tools and update flash script helper

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,10 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
   # Define the packages to be installed in the environment
@@ -8,12 +14,22 @@
     dfu-util
     dfu-programmer
     git
+    clang-tools
   ];
 
   # Custom scripts (aliases) available inside the environment
   scripts = {
-    build-xela.exec = "qmk compile -kb keychron/q3_max/ansi_encoder -km xela";
-    flash-xela.exec = "qmk flash -kb keychron/q3_max/ansi_encoder -km xela";
+    build-xela.exec = "qmk compile -kb keychron/q3_max/ansi_encoder -km xela --compiledb";
+    flash.exec = ''
+      qmk flash -kb keychron/q3_max/ansi_encoder -km xela
+      echo ""
+      echo "Waiting 5 seconds. If your keyboard works, press Ctrl+C to cancel default reflash."
+      sleep 5
+        echo ""
+        echo "No cancel detected within 5 seconds. offering to reflashing default binary..."
+        qmk flash keychron_q3_max_ansi_encoder_default.bin
+      fi
+    '';
   };
 
   # Enter hook to set up environment variables or run scripts when shell starts
@@ -23,7 +39,7 @@
     echo "========================================="
     echo "Available custom commands:"
     echo "  build-xela - Compiles the Q3 Max keymap"
-    echo "  flash-xela - Compiles and flashes keymap"
+    echo "  flash      - Compiles and flashes keymap, then offers to reflash default bin"
     echo "========================================="
     qmk --version
   '';
