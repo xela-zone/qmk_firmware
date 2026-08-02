0c8f20629dae9d8da50b1d03fec385d54d14a32c
Xela <git@xela.zone>
2026-08-01 22:40:13 -0800

    feat(keymap/xela): configure lumino timeout, tap dance lock, and custom rgb initialization

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/xela/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/xela/keymap.c
@@ -17,6 +17,7 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
+
 enum layers {
     BASE,
     FN,
@@ -24,23 +25,28 @@ enum layers {
 
 #define FN_KEY MO(FN)
 
+// Tap dance declarations
+enum tap_dance_actions {
+    TD_LOCK,
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_tkl_ansi(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MUTE,  KC_PSCR,  KC_CTANA, UG_NEXT,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MUTE,  KC_PSCR,  TD(TD_LOCK),        LUMINO,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,             KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,
-        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  FN_KEY,   KC_APP,   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  FN_KEY,   KC_APP,   KC_RCTL,  KC_LEFT,  KC_DOWN,            KC_RGHT),
 
     [FN] = LAYOUT_tkl_ansi(
-        QK_BOOT,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  UG_VALD,  UG_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  UG_TOGG,  _______,  _______,  UG_TOGG,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        UG_TOGG,  UG_NEXT,  UG_VALU,  UG_HUEU,  UG_SATU,  UG_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_CLEAR_EEPROM, _______,
+        QK_BOOT,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  _______,  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  LUMINO,   _______,  _______,            LUMINO,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        LUMINO,   UG_NEXT,  UG_VALU,  UG_HUEU,  UG_SATU,  UG_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_CLEAR_EEPROM,    _______,
         _______,  UG_PREV,  UG_VALD,  UG_HUED,  UG_SATD,  UG_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,            _______,  _______,  _______,  _______,  BAT_LVL,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______),
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,            _______),
 };
 
 // clang-format on
@@ -51,14 +57,51 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 #endif // ENCODER_MAP_ENABLE
 
-#if defined(LK_WIRELESS_ENABLE) || defined(KC_BLUETOOTH_ENABLE)
-extern uint16_t backlit_disable_time;
-#endif
+// --- Tap Dance: Lock key (hold to lock) ---
+
+typedef enum {
+    TD_LOCK_NONE,
+    TD_LOCK_SINGLE_TAP,
+    TD_LOCK_HOLD,
+} td_lock_state_t;
+
+static td_lock_state_t td_lock_state = TD_LOCK_NONE;
+
+static td_lock_state_t td_lock_get_state(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_LOCK_SINGLE_TAP;
+        return TD_LOCK_HOLD;
+    }
+    return TD_LOCK_NONE;
+}
+
+void td_lock_finished(tap_dance_state_t *state, void *user_data) {
+    td_lock_state = td_lock_get_state(state);
+    if (td_lock_state == TD_LOCK_HOLD) {
+        lumino_sleep_soon();
+        tap_code16(LCTL(LSFT(KC_ESC)));
+    }
+    // Single tap: reserved for future use
+}
+
+void td_lock_reset(tap_dance_state_t *state, void *user_data) {
+    td_lock_state = TD_LOCK_NONE;
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LOCK] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_lock_finished, td_lock_reset),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return true;
+}
 
 void keyboard_post_init_user(void) {
-#if defined(LK_WIRELESS_ENABLE) || defined(KC_BLUETOOTH_ENABLE)
-    backlit_disable_time = 300;
-#endif
+    rgb_matrix_mode(RGB_MATRIX_COMMUNITY_MODULE_PALETTEFX_FLOW);
+    uint8_t palette_index = PALETTEFX_AFTERBURN;
+    rgb_matrix_sethsv(RGB_MATRIX_HUE_STEP * palette_index, 255, 255);
+    rgb_matrix_set_speed_noeeprom(128);
+    rgb_matrix_enable_noeeprom();
 }
 
 static const uint16_t dangerous_keys[] = {
